cc2f83d49ac870483a4f566873c80a950b59c559
Andrew <andrewmgunn26@gmail.com>
2021-09-27 09:59:03 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicketClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
