eab23485e98479de60c9539e94e20fbe41cdec04
Andrew <andrewmgunn26@gmail.com>
2021-10-17 19:22:08 -0500

    Ticket consolidation into 1 file - b4 changing of File stuff

--- Ticket.cs
+++ Ticket.cs
@@ -1,6 +1,8 @@
-﻿namespace TicketClasses
+﻿using System;
+
+namespace TicketClasses
 {
-    public class Ticket
+    public abstract class Ticket
     {
         public Ticket(string ticketId = null, string summary = null, string status = null, string priority = null,
             string submitter = null, string assigned = null, string watching = null, string severity = null)
@@ -26,12 +28,124 @@
         public string Submitter { get; set; }
         public string Assigned { get; set; }
         public string Watching { get; set; }
-        
+
         public string Severity { get; set; }
 
         public override string ToString()
         {
             return $"{TicketID},{Summary},{Status},{Priority},{Submitter},{Assigned},{Watching}";
+        }
+    }
+
+    public class Bug : Ticket
+    {
+        public Bug(string ticketId = null, string summary = null, string status = null, string priority = null,
+            string submitter = null, string assigned = null, string watching = null, string severity = null) : base(
+            ticketId, summary, status, priority, submitter, assigned, watching, severity)
+        {
+        }
+
+        public Bug()
+        {
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+
+    public class Enhancement : Ticket
+    {
+        public Enhancement(string ticketId = null, string summary = null, string status = null, string priority = null,
+            string submitter = null, string assigned = null, string watching = null, string software = null,
+            double cost = default, string reason = null, double estimate = default)
+        {
+            TicketID = ticketId;
+            Summary = summary;
+            Status = status;
+            Priority = priority;
+            Submitter = submitter;
+            Assigned = assigned;
+            Watching = watching;
+            Software = software;
+            Cost = cost;
+            Reason = reason;
+            Estimate = estimate;
+        }
+
+        public Enhancement()
+        {
+        }
+
+        public string TicketID { get; set; }
+        public string Summary { get; set; }
+        public string Status { get; set; }
+        public string Priority { get; set; }
+        public string Submitter { get; set; }
+        public string Assigned { get; set; }
+        public string Watching { get; set; }
+
+        public string Software { get; set; }
+
+        public double Cost { get; set; }
+
+        public string Reason { get; set; }
+
+        public double Estimate { get; set; }
+
+        public override string ToString()
+        {
+            return
+                $"{TicketID},{Summary},{Status},{Priority},{Submitter},{Assigned},{Watching}, {Software}, {Cost:C}, {Reason}, {Estimate:N} ";
+        }
+    }
+
+    public class Task : Ticket
+    {
+        public Task(string ticketId = null, string summary = null, string status = null, string priority = null,
+            string submitter = null, string assigned = null, string watching = null, string severity = null) : base(
+            ticketId, summary, status, priority, submitter, assigned, watching, severity)
+        {
+        }
+
+
+        public Task(string ticketId = null, string summary = null, string status = null, string priority = null,
+            string submitter = null, string assigned = null, string watching = null, string projectName = null,
+            DateTime dueDate = default)
+        {
+            TicketID = ticketId;
+            Summary = summary;
+            Status = status;
+            Priority = priority;
+            Submitter = submitter;
+            Assigned = assigned;
+            Watching = watching;
+            ProjectName = projectName;
+            DueDate = dueDate;
+        }
+
+        public Task()
+        {
+        }
+
+        public string TicketID { get; set; }
+        public string Summary { get; set; }
+        public string Status { get; set; }
+        public string Priority { get; set; }
+        public string Submitter { get; set; }
+        public string Assigned { get; set; }
+        public string Watching { get; set; }
+
+        public string ProjectName { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public override string ToString()
+        {
+            return
+                $"{TicketID},{Summary},{Status},{Priority},{Submitter},{Assigned},{Watching}, {ProjectName}, {DueDate:f}";
         }
     }
 }
--- TicketFile.cs
+++ TicketFile.cs
@@ -6,7 +6,7 @@ using NLog.Web;
 
 namespace TicketClasses
 {
-    public class TicketFile : IDisposable
+    public abstract class TicketFileHandler
     {
         private static Logger logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
 
@@ -16,7 +16,7 @@ namespace TicketClasses
             "1,This is a bug ticket,Open,High,Drew Kjell,Jane Doe,Drew Kjell|John Smith|Bill Jones"
         };
 
-        public TicketFile(string filePath, List<Ticket> ticketsList = null)
+        public TicketFileHandler(string filePath, List<Ticket> ticketsList = null)
         {
             FilePath = filePath;
             TicketsList = ticketsList;
@@ -28,11 +28,54 @@ namespace TicketClasses
         public StreamWriter Writer { get; set; }
         public bool IsCreated { get; set; }
 
-        public void Dispose()
+
+        public void WriteToFile(Ticket ticket)
         {
-            Reader?.Dispose();
-            Writer?.Dispose();
+            Writer = new StreamWriter(FilePath, true);
+
+            // if (!File.Exists(FilePath))
+            // {
+            //     Writer.WriteLine(Preset[0]);
+            //     Writer.WriteLine(Preset[1]);
+            // }
+
+            Writer.WriteLine(ticket.ToString());
+
+            Writer.Close();
         }
+
+        public void ReadFromFile()
+        {
+            Reader = new StreamReader(FilePath);
+
+            while (!Reader.EndOfStream) Console.WriteLine(Reader.ReadLine());
+
+            Reader.Close();
+        }
+    }
+
+    public class TicketFile : TicketFileHandler
+    {
+        private static Logger logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
+        public List<string> Preset = new()
+        {
+            "TicketID,Summary,Status,Priority,Submitter,Assigned,Watching",
+            "1,This is a bug ticket,Open,High,Drew Kjell,Jane Doe,Drew Kjell|John Smith|Bill Jones"
+        };
+
+        public TicketFile(string filePath, List<Ticket> ticketsList = null) : base()
+        {
+            FilePath = filePath;
+            TicketsList = ticketsList;
+        }
+
+        public string FilePath { get; set; }
+        public List<Ticket> TicketsList { get; set; }
+        public StreamReader Reader { get; set; }
+        public StreamWriter Writer { get; set; }
+        public bool IsCreated { get; set; }
+        
 
         public void WriteToFile(Ticket ticket)
         {
